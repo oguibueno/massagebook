9d104cdca2b477da63de4fb1e5ea43260b8618df
Guilherme Bueno <oguibueno@gmail.com>
2024-02-03 22:17:46 +0000

    url added

--- lib/features/business/data/datasources/massagebook_remote_data_source.dart
+++ lib/features/business/data/datasources/massagebook_remote_data_source.dart
@@ -13,29 +13,29 @@ class MassageBookRemoteDataSourceImpl implements MassageBookRemoteDataSource {
 
   final http.Client httpClient;
 
-  final String _baseUrl = '';
+  final String _baseUrl =
+      'https://www.massagebook.com/nxt/pu-api/v1/businesses?limit=5&includes=service_categories,primary_photo&filters=latitude%3D%3D32.7833163,longitude%3D%3D-79.9319664';
 
   @override
   Future<BusinessesDataModel> fetchBusinessesData(int offset) async {
-    const limit = 10;
+    //const limit = 10;
 
-    await Future.delayed(Duration(seconds: 2));
+    // await Future.delayed(Duration(seconds: 2));
 
-    String data = await rootBundle.loadString('lib/fixtures.json');
-    final map = json.decode(data);
-    return BusinessesDataModel.fromJson(map);
+    // String data = await rootBundle.loadString('lib/fixtures.json');
+    // final map = json.decode(data);
+    // return BusinessesDataModel.fromJson(map);
 
-    // mount url with offset and limit
-    // final response = await httpClient.get(
-    //   Uri.parse(_baseUrl),
-    // );
+    final response = await httpClient.get(
+      Uri.parse(_baseUrl),
+    );
 
-    // switch (response.statusCode) {
-    //   case 200:
-    //     final map = json.decode(response.body);
-    //     return BusinessesDataModel.fromJson(map);
-    //   default:
-    //     throw Exception(response.reasonPhrase ?? '');
-    // }
+    switch (response.statusCode) {
+      case 200:
+        final map = json.decode(response.body);
+        return BusinessesDataModel.fromJson(map);
+      default:
+        throw Exception(response.reasonPhrase ?? '');
+    }
   }
 }
